4e3fd937bafaec4fa69dcc52752d5c75144d0308
arcaneleague18 <erenyeager4m@gmail.com>
2024-12-10 18:17:21 +0530

    corrected

--- rr.c
+++ rr.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 struct Process {
-    int id,at,bt,remaining_time,wt,tat,completion_time;
+    int id, at, bt, remaining_time, wt, tat, completion_time;
 };
 
 int main() {
-    int n, i, time_quantum, current_time = 0, completed = 0;
+    int n, i, time_quantum, current_time = 0, completed = 0, found;
     float total_wt = 0, total_tat = 0;
 
     printf("Enter the number of processes: ");
@@ -15,13 +15,13 @@ int main() {
 
     printf("Enter Arrival Time and Burst Time for each process:\n");
     for (i = 0; i < n; i++) {
-        processes[i].id = i + 1; 
+        processes[i].id = i + 1;
         printf("Process %d:\n", processes[i].id);
         printf("Arrival Time: ");
         scanf("%d", &processes[i].at);
         printf("Burst Time: ");
         scanf("%d", &processes[i].bt);
-        processes[i].remaining_time = processes[i].bt; 
+        processes[i].remaining_time = processes[i].bt;
         processes[i].wt = 0;
     }
 
@@ -29,8 +29,10 @@ int main() {
     scanf("%d", &time_quantum);
 
     while (completed < n) {
+        found = 0;
         for (i = 0; i < n; i++) {
             if (processes[i].remaining_time > 0 && processes[i].at <= current_time) {
+                found = 1;
                 if (processes[i].remaining_time > time_quantum) {
                     current_time += time_quantum;
                     processes[i].remaining_time -= time_quantum;
@@ -49,11 +51,14 @@ int main() {
                 }
             }
         }
+        if (!found) {
+            current_time++;
+        }
     }
 
     printf("\nProcess\tArrival Time\tBurst Time\tCompletion Time\tWaiting Time\tTurnaround Time\n");
     for (i = 0; i < n; i++) {
-        printf("P%d\t%d\t\t%d\t\t%d\t\t%d\t\t%d\n",processes[i].id, processes[i].at, processes[i].bt,processes[i].completion_time, processes[i].wt, processes[i].tat);
+        printf("P%d\t%d\t\t%d\t\t%d\t\t%d\t\t%d\n", processes[i].id, processes[i].at, processes[i].bt, processes[i].completion_time, processes[i].wt, processes[i].tat);
     }
 
     printf("\nAverage Waiting Time: %.2f\n", total_wt / n);
